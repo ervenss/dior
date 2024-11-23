22974f87ebc4ed27b1c5b3358e12f0f546b1ad41
ervenss <natali.kurilina@inbox.ru>
2024-11-23 13:15:50 +0300

    готовый слайдер со стилями

---
+++ dior/js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: 'fade',
+    autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
