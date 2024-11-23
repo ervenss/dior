cc41e48431068601926716688e78658efe3b2e40
ervenss <natali.kurilina@inbox.ru>
2024-11-23 16:39:32 +0300

    готов второй слайдер

--- dior/js/main.js
+++ dior/js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
     // Optional parameters
     effect: 'fade',
     autoplay: {
@@ -11,4 +11,14 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
+  });
+
+  const swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
   });
